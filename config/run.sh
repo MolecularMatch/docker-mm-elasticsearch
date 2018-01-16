a60a2fe52f5f80d7069dd73349751fb7dff5304d
Ryan Smith <rsmith@molecularmatch.com>
2018-01-16 11:30:13 -0800

    fixing memory lock config changing ownership of config.yml

--- config/run.sh
+++ config/run.sh
@@ -5,4 +5,4 @@ export LOCAL_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
 sed -i "s/\HOST_IP/$LOCAL_IP/" /usr/share/elasticsearch/config/elasticsearch.yml
 sed -i "s/\CLUSTER_NAME/$CLUSTER_NAME/" /usr/share/elasticsearch/config/elasticsearch.yml
 
-/usr/share/elasticsearch/bin/elasticsearch
+/usr/local/bin/docker-entrypoint.sh
